493b055afb5b0fad52b8b17c89a5dbe95200c953
Tony Sanchez <tony.ta.sanchez@gmail.com>
2021-06-09 14:32:17 -0500

    mi primer commit

---
+++ registro-notas-service/src/main/java/com/app/notas/RegistroNotasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroNotasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroNotasServiceApplication.class, args);
+	}
+
+}
